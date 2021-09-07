1c6fdd481953b262318c2040641ad266a6ad1d58
unknown <roshkoff1@gmail.com>
2021-09-07 12:10:07 +0200

    initial fill-in, random-switch

--- parsexml.py
+++ parsexml.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+from operator import attrgetter
+import random
 
 tasks = []
 cores = []
@@ -12,8 +14,35 @@ for child in root:
 
     if child.tag == "Platform":
         for g_child in child:
+            
             for g_g_child in g_child:
+                p = g_g_child.attrib
+                p['mcpID'] = g_child.attrib['Id']
                 cores.append(g_g_child.attrib)
 
-print(tasks)
-print(cores)
+
+for item in tasks:
+    coreToBeBinded = random.choice(cores)
+    item['MCP'] = coreToBeBinded['mcpID']
+    item['Core'] = coreToBeBinded['Id']
+    print ('task: ', item)
+    
+    
+#for item in cores:
+    #print('Core: ',item)
+    
+    
+def switchTask (tasks, cores):
+     randomTask = random.choice(tasks)
+     randomCore= random.choice(cores)
+     
+     randomTask['MCP'] = randomCore['mcpID']
+     randomTask['Core'] = randomCore['Id']
+    
+
+for n in range(100):    
+    switchTask(tasks,cores)
+
+print()
+for item in tasks:
+    print ('1task: ', item)
