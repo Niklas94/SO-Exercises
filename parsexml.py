02cef8095b17edf05d22f75841f1052b7a2f92e9
Niklas Thielemann <t.niklas@gmail.com>
2021-09-14 12:05:22 +0200

    implementation of 'skeleton'

--- parsexml.py
+++ parsexml.py
@@ -7,6 +7,8 @@ cores = []
 tree = ET.parse('small.xml')
 root = tree.getroot()
 
+
+# Reading from xml
 for child in root:
     if child.tag == "Application":
         for g_child in child:
@@ -14,35 +16,15 @@ for child in root:
 
     if child.tag == "Platform":
         for g_child in child:
-            
             for g_g_child in g_child:
                 p = g_g_child.attrib
                 p['mcpID'] = g_child.attrib['Id']
                 cores.append(g_g_child.attrib)
 
-
+# Randomize initial solution
 for item in tasks:
     coreToBeBinded = random.choice(cores)
     item['MCP'] = coreToBeBinded['mcpID']
     item['Core'] = coreToBeBinded['Id']
     print ('task: ', item)
     
-    
-#for item in cores:
-    #print('Core: ',item)
-    
-    
-def switchTask (tasks, cores):
-     randomTask = random.choice(tasks)
-     randomCore= random.choice(cores)
-     
-     randomTask['MCP'] = randomCore['mcpID']
-     randomTask['Core'] = randomCore['Id']
-    
-
-for n in range(100):    
-    switchTask(tasks,cores)
-
-print()
-for item in tasks:
-    print ('1task: ', item)
